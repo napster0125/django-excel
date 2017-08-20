0cd71762b58dad6925008c380f70656a1245c6cc
Napster <napster0125@gmail.com>
2017-08-20 17:51:55 +0530

    Added Level 1 View

--- playExcel/binbash/views.py
+++ playExcel/binbash/views.py
@@ -8,6 +8,8 @@ import json
 
 from common.models import User
 from .models import bbplayer, bblevel
+
+import json
 # Create your views here.
 
 @csrf_exempt
@@ -22,18 +24,46 @@ def bbHome(request) :
     defaults={'playerLevel' : 1, 'playerQn' : 1},
     ) 
     print(playerObj.playerQn)
+    status = True
+    requireHead = ['1-1', '1-4']
     levelObj = bblevel.objects.get(levelId = playerObj.playerLevel, qnId = playerObj.playerQn)
     if request.POST :
-        playerAns = request.POST['answer']
-        print(playerAns)
-        print(levelObj.qnAns)
-        if playerAns == levelObj.qnAns :
-            print("Success!")
-            if playerObj.playerQn == 5 :
-                playerObj.playerLevel = playerObj.playerLevel + 1
-                playerObj.playerQn = 1
+        if 'answer' in request.POST :
+            playerAns = request.POST['answer']
+            print(playerAns)
+            print(levelObj.qnAns)
+        
+            if playerAns == levelObj.qnAns :
+                print("Success!")
+                if playerObj.playerQn == 5 :
+                    playerObj.playerLevel = playerObj.playerLevel + 1
+                    playerObj.playerQn = 1
+                else :
+                    playerObj.playerQn = playerObj.playerQn + 1
+                playerObj.save()
             else :
-                playerObj.playerQn = playerObj.playerQn + 1
-            playerObj.save()
-    return render(request, 'binbash.html', {'problem' : levelObj.qnDesc})
+                status = False
+        if 'proceed' in request.POST :
+            status = False
+      
+    else :
+        status = True
+    print(request.POST)
+    print(status)
+    json_data = open('binbash/templates/intro.json')
+    data = json.load(json_data)
+
+    for d in data :
+        if d['level'] == playerObj.playerLevel and d['question'] == playerObj.playerQn :
+            print(d) 
+            context = d
+
+    levelqn = str(playerObj.playerLevel) + '-' + str(playerObj.playerQn) 
+    print(levelqn)
+    if(levelqn in requireHead) and (status == True) :
+        return render(request, 'bbhead.html', context)
+
+    levelObj = bblevel.objects.get(levelId = playerObj.playerLevel, qnId = playerObj.playerQn)
+
+    return render(request, 'bbhome.html', {'problem' : levelObj.qnDesc})
 
