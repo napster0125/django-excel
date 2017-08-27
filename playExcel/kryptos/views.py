7bb73320e4af208e6c568777f173a3543846e31d
Napster <napster0125@gmail.com>
2017-08-27 17:22:51 +0530

    update level on correct answer

--- playExcel/kryptos/views.py
+++ playExcel/kryptos/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from common.models import User
 from .models import level
 from .models import kryptosuser
 import json
@@ -11,32 +12,45 @@ import json
 # Create your views here.
 @csrf_exempt
 def kryptoshome(request):
-	# print 'haha'
 	#getuser and level ,render clue
-	user = 'usertwo'
+	loginUser = request.session['user']
+
+	usr = User.objects.get(user_id=loginUser)
+	print usr.username
+	print loginUser
 	
-	userobj = kryptosuser.objects.get(user_id=user)
-	levelint = userobj.user_level
-	# print levelint
+	print num
+	usrobj, created = kryptosuser.objects.get_or_create(user_id = usr.user_id,
+    defaults={'user_level' : 1},)
+	levelint = usrobj.user_level
+	#check whether last level
+	last_level = 10
+	if levelint > last_level :
+		levelint = 100
+		#image - 'new levels coming' 
 	#check for advanced levels here
 	levelobj = level.objects.get(level = levelint)
 	response = {'level':levelobj.level,'image':json.dumps(str(levelobj.level_image))}
-	
-	# print levelobj.level_image
-	# return JsonResponse(response)
+	return JsonResponse(response)
 
-	return render(request,'kryptos.html')
+	#return render(request,'kryptos.html')
 
 @csrf_exempt
 def matchanswer(request):
 	data = request.POST
-	user = 'usertwo'
+	# user = 'usertwo'
+	loginUser = request.session['user']
+	print loginUser
+	usr = User.objects.get(user_id=loginUser)
+	print usr.username
 	# print data['answer']
-	curr_level = kryptosuser.objects.get(user_id=user)
+	kryptosplayer = kryptosuser.objects.get(user_id=loginUser)
 	# print curr_level.user_level
-	finalans = level.objects.get(level = curr_level.user_level)
-	if finalans.answer == data['answer']:
+	curr_level = level.objects.get(level = kryptosplayer.user_level)
+	if curr_level.answer == data['answer']:
 		state = True
+		kryptosplayer.user_level = kryptosplayer.user_level + 1
+		kryptosplayer.save()
 	else:
 		state = False	
 
@@ -44,5 +58,10 @@ def matchanswer(request):
 	return JsonResponse(response)
 
 	# return render(request,'kryptos.html')
+@csrf_exempt
+def ranking(request):
+	#to find top five positions
+	
+	return True
 
 
