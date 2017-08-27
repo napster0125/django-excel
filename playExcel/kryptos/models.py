c02683556c9ecc57647d1c0c17e83f4923ba0d66
Napster <napster0125@gmail.com>
2017-08-27 21:41:12 +0530

    model updated for foreign key, new model for submitted answers

--- playExcel/kryptos/models.py
+++ playExcel/kryptos/models.py
@@ -2,15 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from common.models import User
 # Create your models here.
 
 class level(models.Model):
 	level = models.IntegerField(default =1)
 	answer = models.TextField(max_length=250)
-	level_image =  models.ImageField(upload_to = 'level_images/')
+	level_image =  models.FileField(upload_to = 'level_images/')
 	
 
 class kryptosuser(models.Model):
-	user_id = models.CharField(primary_key=True,max_length=100)
+	user_id = models.OneToOneField(User,primary_key=True, on_delete=models.CASCADE)
 	user_level = models.IntegerField(default =1	)
+
+class submittedanswer(models.Model):
+	user_id = models.ForeignKey(kryptosuser,on_delete=models.CASCADE)
+	submitted_answer =  models.TextField()
+	def __unicode__(self):
+		return self.submitted_answer
+
+
