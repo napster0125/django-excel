8f7e4bdce1840cf6c4cdd416e6e84bebbee35c82
Napster <napster0125@gmail.com>
2017-09-02 14:18:48 +0530

    ranklist added

--- playExcel/kryptos/urls.py
+++ playExcel/kryptos/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url,include
 from .views import kryptoshome
 from .views import matchanswer
+from .views import rank
 
 urlpatterns = [
     url(r'submitanswer',matchanswer),
+    url(r'ranklist',rank),
 
     url(r'^', kryptoshome),
     
--- playExcel/kryptos/views.py
+++ playExcel/kryptos/views.py
@@ -26,11 +26,10 @@ def kryptoshome(request):
     defaults={'user_level' : 1,'last_anstime':datetime.datetime.now()},)
 	levelint = usrobj.user_level
 	#check whether last level
-	last_level = 3
+	last_level = 20
 	if levelint > last_level :
 		levelint = 100
 		#image - 'new levels coming' 
-	#check for advanced levels here
 	levelobj = level.objects.get(level = levelint)
 	response = {'level':levelobj.level,'type':levelobj.filetype,'source':levelobj.source_hint ,'image':str(levelobj.level_file)}
 	return JsonResponse(response)
@@ -41,16 +40,13 @@ def kryptoshome(request):
 @isLoggedIn
 def matchanswer(request):
 	data = request.POST
-	# user = 'usertwo'
 	loginUser = request.session['user']
 	print(loginUser)
 	usr = User.objects.get(user_id=loginUser)
 	print(usr.username)
-	# print data['answer']
 	kryptosplayer = kryptosuser.objects.get(user_id=usr.user_id)
 	ans_obj = submittedanswer(user_id = kryptosplayer,submitted_answer = data['answer'])
 	ans_obj.save()
-	# print curr_level.user_level
 	curr_level = level.objects.get(level = kryptosplayer.user_level)
 	if curr_level.answer == data['answer']:
 		state = True
@@ -59,11 +55,25 @@ def matchanswer(request):
 		kryptosplayer.save()
 	else:
 		state = False	
-
 	response = {'valid' : state}
 	return JsonResponse(response)
 
-	# return render(request,'kryptos.html')
+@csrf_exempt
+@isLoggedIn
+def rank(request):
+	loginUser = request.session['user']
+	allkryptosusers =kryptosuser.objects.order_by('user_level','last_anstime')
+	ranklist = []
+	rank=1
+	for userobj in allkryptosusers:
+		user = {'rank':rank,'pic':userobj.user_id.profile_picture,'username':userobj.user_id.username,'level':userobj.user_level}
+		ranklist.append(user)
+		if userobj.user_id.user_id == loginUser:
+			print("Iam user")
+			myrank = rank
+		rank = rank +1
+	response = {'ranklist':ranklist,'myrank':myrank}
+	return JsonResponse(response)
 
 
 
