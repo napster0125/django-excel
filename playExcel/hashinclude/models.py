83bccb8729c3249c523799155ca790ec800b61dc
Napster <napster0125@gmail.com>
2017-09-17 20:00:07 +0530

    Added hashinclude base

--- playExcel/hashinclude/models.py
+++ playExcel/hashinclude/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from common.models import User
 
-# Create your models here.
+class hiuser(models.Model):
+	user_id=models.OneToOneField(User,primary_key=True,on_delete=models.CASCADE)
+	last_sub=models.DateTimeField()
+	points=models.IntegerField()
+	rank=models.IntegerField()
+	def __str__(self):
+		return self.user_id.username
+
+class Submission(models.Model):
+	pid=models.IntegerField(default=1)
+	fid=models.FileField(upload_to='')
+	lang=models.CharField(max_length=9,default='C++')
--- playExcel/hashinclude/views.py
+++ playExcel/hashinclude/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from common.decorators import isLoggedIn,playCookies
+from common.models import User
 
-# Create your views here.
+import datetime
+import json
+
+from .models import Submission
+from .models import hiuser
+from .forms import SubmissionForm
+from .tasks import run
+
+@csrf_exempt
+def submit(request):
+	if request.method=='POST':
+		form=SubmissionForm(request.POST,request.FILES)
+		print(form.errors)
+		if form.is_valid():
+			obj=Submission(pid=1,fid=request.FILES['cfile'],lang=request.POST['lang'])
+			obj.save()
+			run.delay(str(obj.pid),obj.fid.name,obj.lang)
+			return JsonResponse({'result':'Success'})	
+		else:
+			return JsonResponse({'result':'Failed'})
+	else:
+		return render(request,'upload.html')
