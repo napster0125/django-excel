6738e02235bd5ee91c5b0e4004e86024592c4495
Napster <napster0125@gmail.com>
2017-09-20 12:13:36 +0530

    Verify Script and Show Output

--- playExcel/echo/judge.py
+++ playExcel/echo/judge.py
@@ -10,31 +10,30 @@ class judge :
         '''
         self.playerId = playerId
 
-        if not os.path.exists('players') :
-            os.makedirs('players')
+        # if not os.path.exists('echo/players') :
+        #     os.makedirs('echo/players')
 
-        if not os.path.exists('skel') :
-            os.makedirs('skel')
+        # if not os.path.exists('echo/skel') :
+        #     os.makedirs('echo/skel')
 
         self.cwd = os.getcwd()
-
         self.appdir = os.path.join(os.getcwd(), 'players/')
 
         self.workdir = os.path.join(self.appdir, self.playerId+'/')
 
         self.homedir = os.path.join(os.getcwd(), 'skel/home/')
 
-        if not os.path.exists(self.homedir) :
-            os.makedirs(self.homedir)
+        # if not os.path.exists(self.homedir) :
+        #     os.makedirs(self.homedir)
 
-        if not os.path.exists(self.workdir) :
-            os.makedirs(self.workdir)
+        # if not os.path.exists(self.workdir) :
+        #     os.makedirs(self.workdir)
 
-        subprocess.Popen(['cp', '-r' , self.homedir, self.workdir])
+        # subprocess.Popen(['cp', '-r' , self.homedir, self.workdir])
 
         os.chdir(os.path.join(self.workdir, 'home/'))
 
-    def execute(self, level, question, code, arg1, arg2) :
+    def execute(self, level, code, arg1, arg2) :
 
         '''
             Create the shell script from the code and make it executable
@@ -43,46 +42,50 @@ class judge :
 
         '''
         tout = False
-        with open(self.workdir+'home/code.sh', 'w+') as file :
-            file.write(code)
+        codefile = os.path.join(self.workdir, 'home/code.sh')
+        with open(self.workdir+"home/code.sh", "w") as file :
+            # print(code)
+            file.writelines(code)
 
-        subprocess.Popen(['chmod', '+x', self.workdir+'home/code.sh'])
-
+        out = subprocess.Popen(['chmod', '+x', self.workdir+'home/code.sh'])
+   
         with open(self.workdir+'home/output.txt', 'w') as output :
-            out = subprocess.Popen(['rbash', self.workdir+'home/code.sh', arg1], stdout=output)
-
-            try :
-                out.communicate(timeout=1)
-            except out.TimeoutExpired :
-                tout = True
+            with open(self.workdir+'home/error.txt', 'w') as error :
+                # cmd = 
+                out = subprocess.Popen(['bash', codefile], stdout=output, stderr=error)
+                try :
+                    out.communicate(timeout=120)
+                except subprocess.TimeoutExpired :
+                    tout = True
             
-            if tout :
-                output.write('Script Execution Timed Out!')
+                if tout :
+                    error.write('Script Execution Timed Out!')
 
         state = False
-        levelId = str(level) + str(question)
-        testfile = os.path.join(os.path.join(self.cwd, 'testcases/'), levelId+'1.txt')  
+        # levelId = str(level) + str(question)
+        testfile = os.path.join(os.path.join(self.cwd, 'testcases/'), '1test.txt')  
         if not tout :
             state = self.validate(self.playerId, testfile)
 
         if state :
             state = False
             with open(self.workdir+'home/output.txt', 'w') as output :
-                out = subprocess.Popen(['rbash', self.workdir+'home/code.sh', arg2], stdout=output)
+                with open(self.workdir+'home/error.txt', 'w') as error :
+                    out = subprocess.Popen(['rbash', self.workdir+'home/code.sh', arg2], stdout=output, stderr=error)
 
-                try :
-                    out.communicate(timeout=1)
-                except out.TimeoutExpired :
-                    tout = True
+                    try :
+                        out.communicate(timeout=120)
+                    except subprocess.TimeoutExpired :
+                        tout = True
             
-                if tout :
-                    output.write('Script Execution Timed Out!')
+                    if tout :
+                        error.write('Script Execution Timed Out!')
 
-                levelId = str(level) + str(question)
-            testfile = os.path.join(os.path.join(self.cwd, 'testcases/'), levelId+'2.txt')  
+                # levelId = str(level) + str(question)
+            testfile = os.path.join(os.path.join(self.cwd, 'testcases/'), '1test.txt')  
             if not tout :
                 state = self.validate(self.playerId, testfile)
-
+        
         return state
 
     def validate(self, playerId, testfile) :
@@ -99,17 +102,18 @@ def main() :
         Run the judge for the player
 
     '''
-    print(sys.argv[1])
     playerId = sys.argv[1]
     level = sys.argv[2]
-    question = sys.argv[3]
-    code = sys.argv[4]
+    # question = sys.argv[3]
+    # code = '\n'.join(sys.argv[3:])
+    code = sys.argv[3]
 
-    arg1 = sys.argv[5]
-    arg2 = sys.argv[6]
-
+    # with open('/tmp/'+playerId+'.txt', 'r') as temp :
+    #     code = '\n'.join(str(line) for line in temp)
+    arg1 = sys.argv[3]
+    arg2 = sys.argv[4]
     obj = judge(playerId)
-
-    print(obj.execute(level, question, code, arg1, arg2))
+    
+    print(obj.execute(level, code, arg1, arg2))
 
 if __name__ == "__main__" : main()
