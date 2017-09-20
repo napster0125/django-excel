fa0c56c38fd41addbb3b32b7fcc4c1e15879396c
Napster <napster0125@gmail.com>
2017-09-20 12:15:38 +0530

    Remove qnId from models.py

--- playExcel/echo/models.py
+++ playExcel/echo/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class echoplayer(models.Model) :
     playerId = models.CharField(primary_key = True, max_length = 100)
     playerLevel = models.IntegerField(default = 1)
-    playerQn = models.IntegerField(default = 1)
+    # playerQn = models.IntegerField(default = 1)
     partCode = models.CharField(max_length = 1000, default = '')
     ansTime = models.DateTimeField(auto_now = True)
 
@@ -16,14 +16,15 @@ class echoplayer(models.Model) :
         return str(self.playerId)
 
 class echolevel(models.Model) :
-    levelId = models.IntegerField(default = 1)
-    qnId = models.IntegerField(default = 1)
+    levelId = models.IntegerField(primary_key = True, default = 1)
+    # qnId = models.IntegerField(default = 1)
     qnDesc = models.TextField()
     testArg1 = models.CharField(default = '', max_length = 100)
     testArg1 = models.CharField(default = '', max_length = 100)
     
     def __str__(self) :
-        return str(self.levelId) + '-' + str(self.qnId)
+        # return str(self.levelId) + '-' + str(self.qnId)
+        return str(self.levelId)
     
-    class Meta :
-        unique_together = ('levelId', 'qnId')
+    # class Meta :
+    #     unique_together = ('levelId', 'qnId')
