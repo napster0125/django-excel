bda108da2bbc735737af2e3651fc074fa78ac18e
Napster <napster0125@gmail.com>
2017-09-22 01:37:30 +0530

    Revert Changes

--- playExcel/kryptos/admin.py
+++ playExcel/kryptos/admin.py
@@ -2,12 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-# from .models import level
-# from .models import kryptosuser
-# from .models import submittedanswer
+from .models import level
+from .models import kryptosuser
+from .models import submittedanswer
 
 
-# # Register your models here.
-# admin.site.register(level)
-# admin.site.register(kryptosuser)
-# admin.site.register(submittedanswer)
+# Register your models here.
+admin.site.register(level)
+admin.site.register(kryptosuser)
+admin.site.register(submittedanswer)
