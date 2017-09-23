ff2afc2f538946081c11b171d53bc82896e7ee44
Napster <napster0125@gmail.com>
2017-09-23 19:40:52 +0530

    Fix typo

--- playExcel/hashinclude/admin.py
+++ playExcel/hashinclude/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import problems,hiuser,Submission
+from .models import problems,hiuser,Submission,submissionTask
 admin.site.register(problems)
 admin.site.register(hiuser)
 admin.site.register(Submission)
