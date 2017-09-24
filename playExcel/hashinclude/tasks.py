2e57424251ef46d940e4e28c93ed6144f44e3a63
Napster <napster0125@gmail.com>
2017-09-24 10:45:31 +0530

    Typo fix

--- playExcel/hashinclude/tasks.py
+++ playExcel/hashinclude/tasks.py
@@ -36,7 +36,7 @@ def run(pid,fid,lang,loginUser):
                 usr.tries=usr.tries/primes[pid-1]
                 su=hiuser.objects.order_by('total_points','sub_time')
                 for i in enumerate(su):
-                    if i[1].total_points==usr.total_points and i[1].sub_time==usr.sub_time
+                    if i[1].total_points==usr.total_points and i[1].sub_time==usr.sub_time:
                                newrank=i[0]+1
                 fu=hiuser.objects.filter(rank>usr.rank and rank<=newrank)
                 for i in fu:
