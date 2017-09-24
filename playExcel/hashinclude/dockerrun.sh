f0ca15d40d5d561f34ce7232536a25bfd0df59df
Napster <napster0125@gmail.com>
2017-09-24 10:41:44 +0530

    Added ranking scheme

--- playExcel/hashinclude/dockerrun.sh
+++ playExcel/hashinclude/dockerrun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v $PWD/app/media/:/app/media -it 'judge:latest' python3 run.py $1 $2 $3
+docker run --memory=256M --rm -v $PWD/app/media/:/app/media -it 'judge:latest' python3 run.py $1 $2 $3
--- playExcel/hashinclude/tasks.py
+++ playExcel/hashinclude/tasks.py
@@ -32,7 +32,16 @@ def run(pid,fid,lang,loginUser):
             obj.result="AC"
             hashinclude_channel_push({'result':obj.result},{'score':p.points})
             if usr.tries%primes[pid-1] == 0:
-                usr.points+=p.points 
+                usr.total_points+=p.points 
+                usr.tries=usr.tries/primes[pid-1]
+                su=hiuser.objects.order_by('total_points','sub_time')
+                for i in enumerate(su):
+                    if i[1].total_points==usr.total_points and i[1].sub_time==usr.sub_time
+                               newrank=i[0]+1
+                fu=hiuser.objects.filter(rank>usr.rank and rank<=newrank)
+                for i in fu:
+                    i.rank+=1
+                usr.rank=newrank
         else:
             hashinclude_channel_push({'result':res[0].decode('utf8')},{'score':0})
             obj.result=res[0].decode('utf8')
