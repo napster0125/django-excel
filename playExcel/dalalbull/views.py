f8c74fb22dee44d7e2740a92668c30b7f0afd7c4
Napster <napster0125@gmail.com>
2017-09-25 16:00:24 +0530

    Handshake addded

--- playExcel/dalalbull/views.py
+++ playExcel/dalalbull/views.py
@@ -14,15 +14,17 @@ from common.models import User as HomeUser
 from common.decorators import playCookies
 
 @playCookies
+@csrf_exempt
 def handShake(request):
 	print("Cookies:", request.COOKIES )
+    print("access_token: ",request.POST['access_token'])
 	if 'count' not in request.session:
 		request.session['count'] = 0
 	request.session['count'] += 1
 	return JsonResponse({
 			#'sessionid' : request.session.session_key,
 			#'csrftoken' : getCsrfToken(request),
-			'count' : request.session['count']
+			'count' : request.session['count'],
 		})
 def index(request):
     if 'logged_in' in request.session:
